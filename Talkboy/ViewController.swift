c84c49c2ed5656106516dcbe0aa0b39bf09780fd
Unknown <zebahashimi@gmail.com>
2018-05-08 15:53:08 -0700

    Designing the app
    Added Table View + UI View Controller & setup connections with code

--- Talkboy/ViewController.swift
+++ Talkboy/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var recordTextField: UIButton!
+    
+    @IBOutlet weak var playTextField: UIButton!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var addTextField: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func recordButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func playbuttonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
