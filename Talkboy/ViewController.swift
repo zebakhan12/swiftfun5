6a2afbe58f1ca6854e7edc919c7aabf7dc617669
Unknown <zebahashimi@gmail.com>
2018-05-08 16:18:05 -0700

    Step 1: Starting to add code for record button
    creating an audio session, saving the audio session, and then creating the audio recording from the dictionary of setting.

--- Talkboy/ViewController.swift
+++ Talkboy/ViewController.swift
@@ -6,10 +6,13 @@
 //  Copyright © 2018 Zeba Khan. All rights reserved.
 //
 
+//first step is to add the capability to record audio
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var recordTextField: UIButton!
     
@@ -19,29 +22,42 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var addTextField: UIButton!
     
+    var audioRecorder : AVAudioRecorder?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //1. create an audio session
+        let session = AVAudioSession.sharedInstance()
+        try? session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try? session.overrideOutputAudioPort(.speaker)
+        try? session.setActive(true)
+        
+        //2. URL to save audio (place in file system to save audio)
+        if let basePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+            //create a path component
+            let pathComponents = [basePath, "audio.m4a"]
+            //merge to create audio URL
+            if let audioURL = NSURL.fileURL(withPathComponents: pathComponents) {
+                
+                //3. create some settings
+                
+                //4. create audio recorder
+                
+                audioRecorder = AVAudioRecorder(url: audioURL, settings: <#T##[String : Any]#>)
+                audioRecorder?.prepareToRecord()
+            
+            }
+        }
     }
     
     @IBAction func recordButtonTapped(_ sender: Any) {
     }
     
-    @IBAction func playbuttonTapped(_ sender: Any) {
+    @IBAction func playButtonTapped(_ sender: Any) {
     }
     
     @IBAction func addButtonTapped(_ sender: Any) {
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
