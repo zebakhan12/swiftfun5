fa261cb2bb2a9105503ef4d164222b40085fe60a
Unknown <zebahashimi@gmail.com>
2018-05-08 18:32:36 -0700

    App complete!
    able to delete any files from app

--- Talkboy/TableViewController.swift
+++ Talkboy/TableViewController.swift
@@ -54,26 +54,23 @@ class TableViewController: UITableViewController {
             audioPlayer = try? AVAudioPlayer(data: audioData)
             audioPlayer?.play()
         }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
     
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            //pull source which is the sound and not the title
+            let sound = sounds[indexPath.row]
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                context.delete(sound)
+                getSounds()
+            }
+        }
+    }
 }
+
+
